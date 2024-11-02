591a66605559ae5eb096a020a3a8f063c7f4dfd8
ineedagitaccount <123327018+ineedagitaccount@users.noreply.github.com>
2024-11-02 05:01:43 -0400

    added some more images to be displayed. Added lightbox functionality. Bug fix some gallery interactions like clicking on overlay

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,6 @@
+const images = [];
+let currentImageIndex = 0;
+
 function lightMode(){
     var newTheme = document.querySelector("html").getAttribute("data-theme") === "dark" ? "light" : "dark";
     document.querySelector("html").setAttribute("data-theme", newTheme);
@@ -8,10 +11,73 @@ function changePrimary(event){
     document.querySelector(':root').style.setProperty('--primary-color', newColor);
 }
 
+function expandImage(event){
+    const lightbox = document.getElementById('lightbox');
+    const expandedImg = document.getElementById('expandedImg');
+
+    const img = event.currentTarget.querySelector('img');
+
+    expandedImg.src = img.src;
+    lightbox.style.display = 'flex';
+
+    document.body.style.overflow = 'hidden';
+
+    currentImageIndex = Array.from(event.currentTarget.parentNode.children).indexOf(event.currentTarget);
+    loadImages();
+}
+
+function closeLightbox(){
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+
+    document.body.style.overflow = '';
+}
+
+function loadImages(){
+    const gridItems = document.querySelectorAll('.gallery-item img');
+    images.length = 0;
+    gridItems.forEach(element => {
+        images.push(element.src);
+    });
+}
+
+function nextImage(event){
+    event.stopPropagation();
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    updateImage();
+}
+
+function prevImage(event){
+    event.stopPropagation();
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    updateImage();
+}
+
+function updateImage(){
+    const expandedImg = document.getElementById('expandedImg');
+    expandedImg.src = images[currentImageIndex];
+}
+
+window.addEventListener('beforeunload', function(){
+    sessionStorage.setItem('scrollPosition',window.scrollY);
+});
+
 window.onload = function(){
     const colorPickers = document.getElementsByClassName("color-swatch");
     document.getElementById("theme-toggle").addEventListener("click", lightMode);
     for(let i = 0; i < colorPickers.length; i++){
         colorPickers[i].addEventListener("click", changePrimary);
     }
+
+    const scrollPosition = sessionStorage.getItem('scrollPosition');
+    if(scrollPosition){
+        window.scrollTo(0, parseInt(scrollPosition, 10));
+        sessionStorage.removeItem('scrollPosition');
+    }
 }
+
+// document.querySelectorAll('.gallery-item img').forEach(img => {
+//     img.addEventListener('contextmenu', function(event) {
+//         event.preventDefault();
+//     });
+// });
